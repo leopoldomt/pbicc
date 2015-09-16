7a0452cdacebc22863c3d52189617dd18a6421b2
Paulo Barros <pbsfcin@gmail.com>
2015-09-16 17:31:06 -0300

    Adds Android SAXParser.

---
+++ src/mparser/GetMainActivity.java
@@ -0,0 +1,56 @@
+package mparser;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GetMainActivity {
+
+    static BufferedWriter bf;
+
+    public static String getMainActivity(String manifestPath) throws Exception {
+        SAXParserFactory parserFactor = SAXParserFactory.newInstance();
+        SAXParser parser = parserFactor.newSAXParser();
+        SAXHandler handler = new SAXHandler();
+        FileInputStream fis = new FileInputStream(manifestPath); // Manifest file
+        parser.parse(fis,handler);
+        return handler.activityOutput;
+    }
+
+    // args[0] = Manifest path
+    // args[1] = output path
+    public static void main(String[] args) {
+        try {
+            bf = new BufferedWriter(new FileWriter(args[1], false));
+            String mainActivity = getMainActivity(args[0]);
+            if (mainActivity == null) mainActivity = "android.app.Activity";
+            bf.append(mainActivity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bf.close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ src/mparser/SAXHandler.java
@@ -0,0 +1,50 @@
+package mparser;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+/**
+ * The Handler for SAX Events.
+ */
+class SAXHandler extends DefaultHandler {
+
+    public String activityOutput = null;
+    String pckg = "";
+    String curCompName = null;
+    private String MAIN_STR = "android.intent.action.MAIN";
+
+    @Override
+    // Triggered when the start of tag is found.
+    public void startElement(String uri, String localName, String qName,
+            Attributes attributes) throws SAXException {
+        if (activityOutput != null) return; // Found main activity already.
+
+        switch (qName) {
+            case "manifest":
+                pckg = attributes.getValue("package");
+                break;
+            case "activity":
+                curCompName = attributes.getValue("android:name");
+                if (curCompName.startsWith(".")) {
+                    curCompName = pckg + curCompName;
+                } else if (!curCompName.contains(".")) {
+                     curCompName = pckg + "." + curCompName;
+                }
+                break;
+            case "action":
+                String action = attributes.getValue("android:name");
+                if (action.contains(MAIN_STR)) {
+                    activityOutput = curCompName;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+}
