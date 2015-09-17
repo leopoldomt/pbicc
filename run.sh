1681c19ec9ca19e8bef2820dc0dacec543e475ce
Marcelo d'Amorim <damorim@cin.ufpe.br>
2015-09-17 10:10:15 -0400

    update script to generate class files under bin

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ SRC=$PROJECT_HOME/src
 ICC=$SRC/icc
 LIBS=$PROJECT_HOME/libs
 OUT=$PROJECT_HOME/out
+BIN=$PROJECT_HOME/bin
 
 if [ -z "$1" ]
     then
@@ -24,14 +25,14 @@ for x in `ls $LIBS/*.jar`
 do
     CP=$CP:$x
 done
-find $ICC -name "*.java" | xargs javac -cp $CP 
+find $ICC -name "*.java" | xargs javac -cp $CP -d $BIN
 
 ## this script processes icc for a single app
 echo "*** subject $SUBJECT_NAME ***"
 JAVA_FILES=$OUT/$SUBJECT_NAME-javafiles.txt
 ## our current analysis processes java source
 (cd $SUBJECT_SRC; find . -name "*.java" ) > $JAVA_FILES
-
+CP=$BIN:$CP
 java -cp $CP:$OUT:$SRC icc.Main $JAVA_FILES $SUBJECT_SRC
 
 echo "check directory out for generated files.  For example, file zooborns-graph-summary.txt summarizes the contents of the ICC graph generated"
