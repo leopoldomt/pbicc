666948f4a35c819036a6aa77cc7406eb183ac238
Jeanderson Candido <jeandersonbc@gmail.com>
2016-03-11 16:58:21 -0300

    Terminating execution if any error occur
    Added verification for status code.
    Also, I added a condition to check if the jar file
    was generated.

--- run.sh
+++ run.sh
@@ -6,35 +6,36 @@
 PROJECT_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SRC=$PROJECT_HOME/src
 ICC=$SRC/icc
-LIBS=$PROJECT_HOME/libs
 OUT=$PROJECT_HOME/out
-BIN=$PROJECT_HOME/bin
 
-if [ -z "$1" ]
-    then
-        echo "You must pass the subject's path as input!  For example $> ./run.sh test-data/zooborns/"
-        exit 1
+if [ -z "$1" ]; then
+    echo "You must pass the subject's path as input!  For example $> ./run.sh test-data/zooborns/"
+    exit 1
 fi
+if [ ! -f "target/pb-icc.jar" ]; then
+    echo "Main jar file not found. Run \"mvn clean install\" and try again"
+    exit 1
+fi
+
 SUBJECT_DIR=$1
 SUBJECT_SRC=$SUBJECT_DIR/src/
 SUBJECT_NAME=$(basename $SUBJECT_DIR)
 ## clean-compile code 
 mkdir -p $OUT
-mkdir -p $BIN
 rm -rf $OUT/*
-for x in `ls $LIBS/*.jar`
-do
-    CP=$CP:$x
-done
-find $ICC -name "*.java" | xargs javac -cp $CP -d $BIN
 
 ## this script processes icc for a single app
 echo "*** subject $SUBJECT_NAME ***"
 JAVA_FILES=$OUT/$SUBJECT_NAME-javafiles.txt
+
 ## our current analysis processes java source
 (cd $SUBJECT_SRC; find . -name "*.java" ) > $JAVA_FILES
-CP=$BIN:$CP
-java -cp $CP:$OUT:$SRC icc.Main $JAVA_FILES $SUBJECT_SRC $(find $SUBJECT_DIR -name AndroidManifest.xml)
+
+## execute analysis and check its status code.
+java -jar target/pb-icc.jar $JAVA_FILES $SUBJECT_SRC $(find $SUBJECT_DIR -name AndroidManifest.xml)
+if [ "$?" -ne 0 ]; then
+    exit 1
+fi
 
 echo "check directory out for generated files.  For example, file zooborns-graph-summary.txt summarizes the contents of the ICC graph generated"
 
