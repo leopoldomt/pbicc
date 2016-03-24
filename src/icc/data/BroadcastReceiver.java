ca48385930b78a8bacfa670ec11828b727e29b6a
Leopoldo Teixeira <leopoldomt@gmail.com>
2016-03-24 13:09:08 -0300

    creating Service and BroadcastReceiver classes

---
+++ src/icc/data/BroadcastReceiver.java
@@ -0,0 +1,15 @@
+package icc.data;
+
+public class BroadcastReceiver extends Component {
+
+	public BroadcastReceiver() {
+		super();
+	}
+
+	@Override
+	public String toStringExclusiveAttributes() {
+		return "";
+	}
+
+}
+
---
+++ src/icc/data/Service.java
@@ -0,0 +1,15 @@
+package icc.data;
+
+public class Service extends Component {
+
+	public Service() {
+		super();
+	}
+
+	@Override
+	public String toStringExclusiveAttributes() {
+		return "";
+	}
+
+}
+
