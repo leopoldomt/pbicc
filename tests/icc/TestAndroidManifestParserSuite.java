6669f4e297f021d3850d8cb500c2d503192de31d
jpttrindade <tenoriojpaulo@gmail.com>
2016-03-30 22:47:48 -0300

    create test of Content Provider attributes.

---
+++ tests/icc/TestAndroidManifestParserSuite.java
@@ -0,0 +1,12 @@
+package icc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TestAndroidManifestParserActivity.class,
+		TestAndroidManifestParserService.class,
+		TestAndroidManifestParserContentProvider.class })
+public class TestAndroidManifestParserSuite {
+
+}
