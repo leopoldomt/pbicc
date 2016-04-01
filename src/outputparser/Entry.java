1d825266b904626ea73362ad8b2cc668e6a97e9f
damorim <damorim.marcelo@gmail.com>
2016-04-01 18:49:07 -0400

    adding code to parse json file and compute precision and recall; added example inputs for testing.  **PLEASE TEST THIS MORE**

---
+++ src/outputparser/Entry.java
@@ -0,0 +1,58 @@
+package outputparser;
+
+/**
+ * This is the data structure to represent the output of the analysis.  
+ * 
+ * TODO: Consider building objects of this kind to build JSON files.   
+ * 
+ * @author damorim
+ *
+ */
+
+public class Entry {
+	String scope;
+	String methodType;
+	String identifier;
+	String component;
+	String action;
+	String data;
+	String mimeType;
+	String extras;
+	
+	public Entry(String scope, String methodType) {
+		super();
+		this.scope = scope;
+		this.methodType = methodType;
+	}
+	
+	// consider using reflection for this...
+	public String select(String column) {
+		String res;
+		switch (column) {
+		case "scope":
+			res = scope;
+			break;
+		case "methodType":
+			res = methodType;
+			break;
+		case "identifier":
+			res = identifier;
+			break;
+		case "component":
+			res = component;
+			break;
+		case "action":
+			res = action;
+			break;
+		case "data":
+			res = mimeType;
+			break;
+		case "extras":
+			res = extras;
+			break;
+		default:
+			throw new RuntimeException(String.format("Attribute %s not found!  Please check.", column));
+		}
+		return res;
+	}
+}
