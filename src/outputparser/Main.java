de275d06c13d00f344c89a592c840e0f97b39ac4
Jeanderson Candido <jeandersonbc@gmail.com>
2016-04-06 12:52:05 -0300

    Support for multiple inputs on PrecisionRecall

--- src/outputparser/Main.java
+++ src/outputparser/Main.java
@@ -5,45 +5,51 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
-		// ground truth 
-		String s = "src/outputparser/ground-truth-primitive-ftp-d.json";
-		List<Entry> golden = load(s);
-		
-		// results
-		s = "src/outputparser/fake-output-primitive-ftp-d.json";
-		List<Entry> output = load(s);
-		
-		PairDoubles pr = precisionRecall(golden, output, "component");
-		System.out.printf("(precision, recall) = %s", pr);
+
+		List<String> srcPaths = Arrays.asList("src/outputparser/ground-truth-primitive-ftp-d.json");
+		List<String> gtPaths = Arrays.asList("src/outputparser/fake-output-primitive-ftp-d.json");
+
+		for (int i = 0; i < srcPaths.size(); i++) {
+
+			// ground truth
+			List<Entry> golden = load(gtPaths.get(i));
+
+			// results
+			List<Entry> output = load(srcPaths.get(i));
+
+			PairDoubles pr = precisionRecall(golden, output, "component");
+			System.out.printf("(precision, recall) = %s", pr);
+		}
 	}
 
 	private static PairDoubles precisionRecall(List<Entry> golden, List<Entry> output, String attribute) {
 		PairInts pis = findHits(output, golden, attribute);
 		double precision = ((double) pis.a) / pis.b;
-		
+
 		pis = findHits(golden, output, attribute);
 		double recall = ((double) pis.a) / pis.b;
-		
+
 		return new PairDoubles(precision, recall);
 	}
 
 	/**
-	 * This function returns the number of hits of elements from the pivot 
-	 * dataset (first parameter) against the eval dataset (second paramater), 
-	 * with respect to a given attribute in the dataset.  
+	 * This function returns the number of hits of elements from the pivot
+	 * dataset (first parameter) against the eval dataset (second paramater),
+	 * with respect to a given attribute in the dataset.
 	 * 
-	 * For example, let's assume there is only one attribute in the datasets.  
-	 * For the pivot {a, b, -, -} and eval {b, c}, this function returns the 
+	 * For example, let's assume there is only one attribute in the datasets.
+	 * For the pivot {a, b, -, -} and eval {b, c}, this function returns the
 	 * pair (1, 2) reflecting the fact that there was 1 hit (element b) and the
-	 * effective size of the set was 2 instead of 4.   
+	 * effective size of the set was 2 instead of 4.
 	 * 
 	 * @param pivot
 	 * @param eval
@@ -57,13 +63,14 @@ public class Main {
 			String s = out.select(attribute);
 			if (s.equals("-")) {
 				// consider as if there was no element
-				continue; 
+				continue;
 			}
 			sizeOutput++;
 			for (Entry gold : eval) {
 				// necessary (but not sufficient condition)
 				if (out.scope.equals(gold.scope) && out.methodType.equals(gold.methodType)) {
-					//TODO: may need to replace equals below with something else for data or extras
+					// TODO: may need to replace equals below with something
+					// else for data or extras
 					if (s.equals(gold.select(attribute))) {
 						hitsOutput++; // match!
 						break;
@@ -78,34 +85,39 @@ public class Main {
 	private static List<Entry> load(String s) throws FileNotFoundException, IOException {
 		Gson g = new Gson();
 		Reader reader = new FileReader(s);
-		Type type = new TypeToken<List<Entry>>(){}.getType();
+		Type type = new TypeToken<List<Entry>>() {
+		}.getType();
 		List<Entry> ll = g.fromJson(reader, type);
 		reader.close();
 		return ll;
 	}
-	
+
 	/** helpers **/
-	
-	static class PairInts { 
+
+	static class PairInts {
 		int a, b;
+
 		public PairInts(int a, int b) {
 			this.a = a;
 			this.b = b;
-		}	
+		}
+
 		@Override
-		public String toString() {			
+		public String toString() {
 			return String.format("(%d, %d)", a, b);
 		}
 	}
-	
-	static class PairDoubles { 
+
+	static class PairDoubles {
 		double p, r;
+
 		public PairDoubles(double p, double r) {
 			this.p = p;
 			this.r = r;
-		} 
+		}
+
 		@Override
-		public String toString() {			
+		public String toString() {
 			return String.format("(%.2f, %.2f)", p, r);
 		}
 	}
