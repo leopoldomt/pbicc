dd7bf2f1eec75c11b7fcd975b2374f62bab563df
jpttrindade <tenoriojpaulo@gmail.com>
2016-05-26 05:28:49 -0300

    improve intent resolution implementation

--- src/icc/intent/IntentResolution.java
+++ src/icc/intent/IntentResolution.java
@@ -13,9 +13,20 @@ public class IntentResolution {
 	
 
 	public static Result resolve(IntentForResolution it, Component comp) {
-		// System.out.println(comp.name);
+		
 		Result response = new Result();
 		response.match = false;
+
+		if(it.getComponentName() != null) {
+			if(it.getComponentName().equals(comp.name)) {
+				response.match = true;
+				return response;
+			} else {
+				response.reason = String.format("Explict Intent: not match(%s x %s)", it.getComponentName(), comp.name);
+				return response;
+			}
+		} 
+
 		for (IntentFilter ifilter : comp.intentFilters) {
 			// System.out.println(">>>> Start intent-fiter");
 
@@ -52,55 +63,122 @@ public class IntentResolution {
 			throws ActionTestException {
 		// System.out.println(ifilter.action);
 		if (0 == ifilter.actions.size()) {
-			throw new ActionTestException();
+			throw new ActionTestException("Intent-Filter without actions.");
 		}
 
 		if (null != it.getAction()) {
 			//if (!it.getAction().equals(ifilter.action)) {
 			if(!ifilter.actions.contains(it.getAction())){
-				throw new ActionTestException();
+				throw new ActionTestException("Intent-Filter not contains any Intent's actions.");
 			}
 		}
 
 	}
+	
 
 	private static void dataTest(IntentForResolution it, IntentFilter ifilter)
 			throws DataTestException {
-		// System.out.println(ifilter.data);
-
-		DataTestException e = new DataTestException();
-		if (it.getData().scheme == null && it.getData().mimeType == null) {
-
-			if (ifilter.data.scheme != null || ifilter.data.mimeType != null) {
-				throw e;
+		if (it.getData().scheme != null && ifilter.data.scheme != null) {
+			// i(+,_) 
+			// f(+,_)
+			if (it.getData().mimeType != null && ifilter.data.mimeType == null) {
+				// i(+,+) 
+				// f(+,-)
+				throw new DataTestException("intent has mimetype, ifilter hasn't mimetype.");
 			}
-			// TODO nothing?
-
-		} else if (it.getData().scheme != null && it.getData().mimeType == null) {
-
-			if (ifilter.data.mimeType == null
-					&& !matchUri(it.getData(), ifilter.data)) {
-				throw e;
+			
+			if (it.getData().mimeType == null && ifilter.data.mimeType != null) {
+				// i(+,-) 
+				// f(+,+)
+				throw new DataTestException("intent hasn't mimetype, ifilter has mimetype.");
 			}
-
-		} else if (it.getData().scheme == null && it.getData().mimeType != null) {
-			if (ifilter.data.scheme != null
-					|| !matchMimetype(it.getData(), ifilter.data)) {
-				throw e;
+			// i(+,+) 
+			// f(+,+)
+			
+			if (!matchUri(it.getData(), ifilter.data)){
+				throw new DataTestException("uri not match.");
 			}
-		} else {
-			// (it.getData().scheme != null && it.getData().mimeType != null)
-			if (ifilter.data.mimeType == null
-					|| !(ifilter.data.scheme == null && (it.getData().scheme
-							.equals("content") || it.getData().scheme
-							.equals("file")))
-					|| (!matchMimetype(it.getData(), ifilter.data) || !matchUri(
-							it.getData(), ifilter.data))) {
-				throw e;
+			
+			if (!matchMimetype(it.getData(), ifilter.data)) {
+				throw new DataTestException("mimeType not match.");
 			}
-			// ....
+			return;
+		} 
+		
+		if (it.getData().scheme != null && ifilter.data.scheme == null) {
+			// i(+,_) 
+			// f(-,_)
+			
+			if (it.getData().mimeType != null && ifilter.data.mimeType == null) {
+				// i(+,+) 
+				// f(-,-)
+				throw new DataTestException("filter hasn't data.");
+			}
+			
+			if (it.getData().mimeType == null && ifilter.data.mimeType == null) {
+				// i(+,-) 
+				// f(-,-)
+				throw new DataTestException("filter hasn't data.");
+			}
+			
+			
+			if (it.getData().mimeType == null && ifilter.data.mimeType != null) {
+				// i(+,-) 
+				// f(-,+)
+				throw new DataTestException("intent has uri but not mimeType. ifilter has mimeType but not uri.");
+			}
+			
+			// i(+,+) 
+			// f(-,+)
+						
+			if (it.getData().scheme.equals("file") || it.getData().scheme.equals("content") ) {
+				throw new DataTestException("intent uri.scheme isn't <file> neither <content>.");
+			}
+			
+			if (!matchMimetype(it.getData(), ifilter.data)) {
+				throw new DataTestException("mimeType not match.");
+			}		
+			return;
 		}
+		
+		
+		if (it.getData().scheme == null && ifilter.data.scheme != null) {
+			// i(-,_) 
+			// f(+,_)
+			throw new DataTestException("intent hasn't uri and filter has it.");
+		}
+		
+		if (it.getData().scheme == null && ifilter.data.scheme == null) {
+			// i(-,_) 
+			// f(-,_)
+			
+			if (it.getData().mimeType != null && ifilter.data.mimeType == null) {
+				// i(-,+) 
+				// f(-,-)
+				throw new DataTestException("filter hasn't data.");
+			}
+			
+			if (it.getData().mimeType == null && ifilter.data.mimeType != null) {
+				// i(-,-) 
+				// f(-,+)				
+				throw new DataTestException("intent hasn't data.");
+			}
+			
+			
+			if (it.getData().mimeType != null && ifilter.data.mimeType != null){
+				// i(-,+) 
+				// f(-,+)
+				if(!matchMimetype(it.getData(), ifilter.data)) {
+					throw new DataTestException("mimeType not match.");
+				}
+			}
+			
+			// i(-,-) 
+			// f(-,-)
 
+			return;
+		}
+		
 	}
 
 	private static boolean matchMimetype(IntentFilter.Data dt1,
@@ -124,22 +202,24 @@ public class IntentResolution {
 
 	}
 
-	private static boolean matchUriPath(IntentFilter.Data dt1,
-			IntentFilter.Data dt2) {
+	private static boolean matchUriPath(IntentFilter.Data intentData,
+			IntentFilter.Data filterData) {
+		//TODO A path specification can contain a wildcard asterisk (*) to require only 
+		//a partial match of the path name.
 		boolean match = false;
-		if (dt1.path == null && dt2.path == null) {
+		if (intentData.path == null && filterData.path == null) {
 			match = true;
-		} else if (dt1.path != null && dt2.path != null) {
-			if (dt1.path.equals(dt2.path)) {
-				if (dt1.pathPrefix == null && dt1.pathPrefix == null) {
+		} else if (intentData.path != null && filterData.path != null) {
+			if (intentData.path.equals(filterData.path)) {
+				if (intentData.pathPrefix == null && intentData.pathPrefix == null) {
 					match = true;
-				} else if (dt1.pathPrefix != null && dt2.pathPrefix != null) {
-					if (dt1.pathPrefix.equals(dt2.pathPrefix)) {
-						if (dt2.pathPattern == null && dt2.pathPattern == null) {
+				} else if (intentData.pathPrefix != null && filterData.pathPrefix != null) {
+					if (intentData.pathPrefix.equals(filterData.pathPrefix)) {
+						if (filterData.pathPattern == null && filterData.pathPattern == null) {
 							match = true;
-						} else if (dt1.pathPattern != null
-								&& dt2.pathPattern != null) {
-							if (dt1.pathPattern.equals(dt2.pathPattern)) {
+						} else if (intentData.pathPattern != null
+								&& filterData.pathPattern != null) {
+							if (intentData.pathPattern.equals(filterData.pathPattern)) {
 								match = true;
 							}
 						}
@@ -147,29 +227,43 @@ public class IntentResolution {
 
 				}
 			}
+		} else {
+			if(filterData.path == null){
+				match = true;
+			}
 		}
 		return match;
 	}
 
-	private static boolean matchUri(IntentFilter.Data dt1, IntentFilter.Data dt2) {
+	private static boolean matchUri(IntentFilter.Data intentData, IntentFilter.Data filterData) {
+
 		boolean match = false;
-		if (dt1.scheme == null && dt2.scheme == null) {
+		
+		
+		if (intentData.scheme == null && filterData.scheme == null) {
 			match = true;
-		} else if (dt1.scheme != null && dt2.scheme != null) {
-			if (dt1.scheme.equals(dt2.scheme)) {
-				if (dt1.host != null && dt2.host != null) {
-					if (dt1.host.equals(dt2.host)) {
-						if (dt1.port == null && dt2.port == null
-								|| dt1.port != null && dt2.port != null) {
-							match = matchUriPath(dt1, dt2);
+		} else if (intentData.scheme != null && filterData.scheme != null) {
+			if (intentData.scheme.equals(filterData.scheme)) {
+				if (intentData.host != null && filterData.host != null) {
+					if (intentData.host.equals(filterData.host)) {
+
+						if (intentData.port == null && filterData.port == null) {
+							if (intentData.path != null && filterData.path != null) {
+								match = matchUriPath(intentData, filterData);	
+							}
+						} else {
+							if (intentData.port != null && filterData.port != null){
+								if(intentData.port.equals(filterData.port)) {
+									match = matchUriPath(intentData, filterData);
+								}
+							}
 						}
 					}
-				} else if (dt1.host == null && dt2.host == null) {
+				} else if (filterData.host == null) {
 					match = true;
 				}
-
 			}
-		}
+		} 
 		return match;
 
 	}
@@ -183,38 +277,73 @@ public class IntentResolution {
 			if (ifilter.categories.size() > 0) {
 				for (String ctg : it.getCategories()) {
 					if (!ifilter.categories.contains(ctg)) {
-						throw new CategoryTestException();
+						throw new CategoryTestException("Intent-Filter not contais Intent's category.");
 					}
 				}
 			} else {
-				throw new CategoryTestException();
+				throw new CategoryTestException("Intent-Filter hasn't categories.");
 			}
 		} else if (ifilter.categories.size() > 0) {
 			
 		} else {
 			// TODO what happen when has no category on intent neither on
 			// intent-filter?
-			throw new CategoryTestException();
+			throw new CategoryTestException("Intent-Filter not contais Intent's category. Intent hasn't too.");
 		}
 
 	}
 
 	static class ActionTestException extends Exception {
-		public ActionTestException() {
+		public String msg;
+		
+		private ActionTestException() {
 			super("ActionTestException");
+		}
+		public ActionTestException(String msg) {
+			this();
+			this.msg = msg;
+		}
+		
+		@Override
+		public String getMessage() {
+			// TODO Auto-generated method stub
+			return super.getMessage() + " - "+msg;
 		}
 	}
 
 	static class DataTestException extends Exception {
-		public DataTestException() {
-			super("DataTesdtException");
+		public String msg;
+
+		private DataTestException() {
+			super("DataTestException");
+		}
+		public DataTestException(String msg) {
+			this();
+			this.msg = msg;
+		}
+		
+		@Override
+		public String getMessage() {
+			// TODO Auto-generated method stub
+			return super.getMessage() + " - "+msg;
 		}
 
 	}
 
 	static class CategoryTestException extends Exception {
-		public CategoryTestException() {
-			super("CategoryException");
+		public String msg;
+
+		private CategoryTestException() {
+			super("CategoryTestException");
+		}
+		public CategoryTestException(String msg) {
+			this();
+			this.msg = msg;
+		}
+		@Override
+		public String getMessage() {
+			// TODO Auto-generated method stub
+			return super.getMessage() + " - "+msg;
 		}
 	}
 
